298ff6a01c748f2b142d362ac47e7be708f7c036
swedishsailor <kzygowski7@gmail.com>
2021-12-30 17:40:25 +0100

    webpack init

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Test import of a JavaScript module
+import { example } from '@/js/main'
+
+// Test import of styles
+import '@/styles/index.scss'
+
+
---
+++ src/js/data.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+        id:0,
+        name:`1`,
+        img:`1`,
+        href:`1`,
+    },
+    {
+        id:1,
+        name:`2`,
+        img:`2`,
+        href:`2`,
+    }
+]
