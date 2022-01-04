6c9e895e727e4bd837c66fffd9877519a7c25635
swedishsailor <kzygowski7@gmail.com>
2022-01-04 17:59:28 +0100

    get and post data works perfectly on localhost

--- src/js/createRecipe.js
+++ src/js/createRecipe.js
@@ -1,8 +1,12 @@
 import { generalContainer } from "./main";
+import axios from 'axios';
 
 /* Variables */
 // Number of steps to choose in creator
 const nr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// Data endpoint
+const url='http://localhost:4000';
+const postUrl = 'http://localhost:4000/data';
 
 // Query button
 const createRecipeButton = document.querySelector('.createRecipeButton');
@@ -12,7 +16,7 @@ const createRecipeButton = document.querySelector('.createRecipeButton');
 // Click the nav button
 createRecipeButton.addEventListener('click', () => {
     generalContainer.innerHTML = '';
-    generalContainer.innerHTML += '<div class="createRecipeContainer"><i class="far fa-arrow-alt-circle-left back"></i><h2 class="createRecipeTitle">Create recipe!</h2><div class="row"><label>Name :</label><input placeholder="type name of recipe..." class="nameInput" type="text"/></div><div class="row"><label>Description :</label><textarea class="descriptionInput" placeholder="" type="textarea" rows="4" cols="50"></textarea></div><div class="row"><label>Ingredients :</label><input class="ingredientsInput" type="text" placeholder="pepper, salt, sushi rice, wasabi"/></div><div class="row"><label>Number of steps :</label><select class="selectSteps">' + nr.map(i => `<option class="stepInput" value="${i}">${i}</option>`) + '</select></div><div class="row stepDiv">' + `<label class="steps">Step ${1}<input class="stepsText"/>` + '</div><div class="row"><label>Small image :</label><input class="smallImageInput" placeholder="paste here image url" type="text"/></div><div class="row"><label>Big image :</label><input placeholder="paste here image url" class="bigImageInput" type="text"/></div><div class="row"><button type="submit">Create</buton></div></div>';
+    generalContainer.innerHTML += '<div class="createRecipeContainer"><i class="far fa-arrow-alt-circle-left back"></i><h2 class="createRecipeTitle">Create recipe!</h2><form '+ /*action="http://localhost:4000/data"*/ '><div class="row"><label>Name :</label><input placeholder="type name of recipe..." class="nameInput" type="text"/></div><div class="row"><label>Description :</label><textarea class="descriptionInput" placeholder="" type="textarea" rows="4" cols="50"></textarea></div><div class="row"><label>Ingredients :</label><input class="ingredientsInput" type="text" placeholder="pepper, salt, sushi rice, wasabi"/></div><div class="row"><label>Number of steps :</label><select class="selectSteps">' + nr.map(i => `<option class="stepInput" value="${i}">${i}</option>`) + '</select></div><div class="row stepDiv">' + `<label class="steps">Step ${1}<input class="stepsText"/>` + '</div><div class="row"><label>Small image :</label><input class="smallImageInput" placeholder="paste here image url" type="text"/></div><div class="row"><label>Big image :</label><input placeholder="paste here image url" class="bigImageInput" type="text"/></div><div class="row"><button type="submit" class="createRecipeButton">Create</buton></div></form></div>';
 
 })
 
@@ -31,11 +35,91 @@ const renderSteps = (value) => {
 }
 
 
-// event delegation on change then umber of steps in select
+// event delegation on change then umber of steps in select then render number of steps under
 generalContainer.addEventListener('change', (e) => {
     if(e.target.classList.contains('selectSteps')){
     console.log(e.target.value);
     renderSteps(e.target.value);
     }
+})
+
+const getData = () => {
+    axios.get(url+'/data')
+    .then(data=>console.log(data.data))
+    .catch(err=>console.log(err))
+}
+
+const postData = (payload) => {
+    axios.post(url+'/datapost', {
+        payload
+    })
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
+}
+
+const postIngredients = (payload) => {
+    axios.post(url+'/ingredientspost', {
+        payload
+    })
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
+}
+
+const postSteps = (payload) => {
+    axios.post(url+'/stepspost', {
+        payload
+    })
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err))
+}
+
+
+generalContainer.addEventListener('click', (e, name,description, ingredients, steps, smallImage, bigImage) => {
+        // create button click
+        if(e.target.classList.contains('createRecipeButton')){
+            // GET data http request
+            getData();
+           name = generalContainer.querySelector('.nameInput').value; 
+           description = generalContainer.querySelector('.descriptionInput').value; 
+           ingredients = generalContainer.querySelector('.ingredientsInput').value.split(',');
+           steps =[];
+           // make an array out of all steps inputs to push every element
+           generalContainer.querySelectorAll('.stepsText').forEach(element => {
+               // get only steps where is text
+               element.value !== '' ? steps.push(element.value) : null
+           })
+           smallImage = generalContainer.querySelector('.smallImageInput').value; 
+           bigImage = generalContainer.querySelector('.bigImageInput').value; 
+           console.log('name: '+name, '\ndescription: ' + description, '\ningredients :' + ingredients, '\nsteps :' + steps, '\nsmallImage: '+ smallImage, '\nbigImage :' + bigImage);
+           // Define payload and add to it all features
+           // POST all data
+           let payload = {};
+           payload.id = 2;
+           payload.name = name;
+           payload.text = description;
+           payload.img = smallImage;
+           payload.bigImg =bigImage;
+           postData(payload);
+
+           // POST all ingredients
+           let ingredientsPayload = {};
+           ingredients.forEach(element => {
+            ingredientsPayload.id = payload.id;
+            ingredientsPayload.name = element;
+            ingredientsPayload.recipeId = payload.id;
+            postIngredients(ingredientsPayload);
+           })
+
+           // POST all steps
+           let stepsPayload = {};
+           steps.forEach(element => {
+               stepsPayload.id = payload.id;
+               stepsPayload.name = element;
+               stepsPayload.stepsId = payload.id;
+               console.log(stepsPayload);
+               postSteps(stepsPayload);
+           })           
+           //console.log(payload);
+        }
 })
 
