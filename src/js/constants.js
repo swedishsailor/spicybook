e46c7981dbb8255166e3be411982227d8f458ace
swedishsailor <kzygowski7@gmail.com>
2022-01-19 03:41:26 +0100

    navbar RWD, login HTML Template added

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,1 +1,9 @@
 export const url='https://spicybook-server.herokuapp.com';
+
+export const loginHTML = '<div class="loginContainer">' + 
+'<i class="far fa-arrow-alt-circle-left back"></i>' +
+'<h2 class="loginTitle">Log in</h2>' +
+'<label>Account e-mail:</label><input class="loginInput" type="email" placeholder="Your e-mail"></input>' +
+'<label>Account password:</label><input class="loginInput" type="password" placeholder="Your password"></input>' +
+'<button type="submit" class="submitButton">Submit</button>'+
+'<p> or </p><a href="#" class="registerLink"> Register an account</a></div>';
